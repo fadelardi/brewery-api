431bb9275c79981188a042b38521315a244b46ef
Frank Adelardi <fmadelardi@gmail.com>
2017-02-21 23:00:02 +0100

    Basic template

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
-<?php
-die('index file');
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Beer App</title>
+</head>
+<body>
+<h3>Distilled SCH Beer App</h3>
+
+<div>
+	<div>
+		<span>Beer Name</span>
+		<div>Image here</div>
+	</div>
+	<div>Lorem Ipsum</div>
+	<div>
+		<button>Another Beer</button>
+		<button>More from this Brewery</button>
+	</div>
+</div>
+
+<h3>Search</h3>
+<div>
+	<div><input type="text" /></div>
+	<div><input type="radio" /> Beer <input type="radio" /> Brewery</div>
+	<div><button>Search</button></div>
+</div>
+
+<h3>Search Results</h3>
+
+<div>
+	<div>
+		<div>Img</div>
+		<div>Desc</div>
+	</div>
+	<div>
+		<div>Img</div>
+		<div>Desc</div>
+	</div>
+	<div>
+		<div>Img</div>
+		<div>Desc</div>
+	</div>
+</div>
+</body>
+</html>
