d1dd0ec438e98c0ed2c2d653b72f4727629418fd
Frank Adelardi <fmadelardi@gmail.com>
2017-02-23 10:45:04 +0100

    Removing debug

--- bootstrap.php
+++ bootstrap.php
@@ -7,11 +7,11 @@ $api = new BreweryApi();
 
 $results = false;
 $action = isset($_GET['action']) ? $_GET['action'] : false;
-die('topkek');
+
 switch($action) {
 	case 'More from this Brewery':
-		// $randomBeer = ['data' => ['name' => 'kek', 'description' => 'topkek'] ];
-		$randomBeer = json_decode($api->getRandomBeerFromBrewery($_GET['bid']), true);
+		$randomBeer = ['data' => ['name' => 'kek', 'description' => 'topkek'] ];
+		// $randomBeer = json_decode($api->getRandomBeerFromBrewery($_GET['bid']), true);
 		$bid = $_GET['bid'];
 		break;
 	case 'Search':
@@ -19,7 +19,7 @@ switch($action) {
 		$type = $_GET['type'];
 		$results = json_decode($api->search($query, $type), true);
 	default:
-		// $randomBeer = ['data' => ['name' => 'kek', 'description' => 'topkek'] ];
+		$randomBeer = ['data' => ['name' => 'kek', 'description' => 'topkek'] ];
 		$randomBeer = json_decode($api->getRandomBeer(), true);
 		$bid = isset($randomBeer['data']['breweries'][0]['id']) ? $randomBeer['data']['breweries'][0]['id'] : 0;
 }
